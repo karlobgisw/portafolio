04a36d3dc09c82a344701b2c8bbd0f5ea6e1ebfc
Karlo Leonardo Barrera Garcia <karlongobg@gmail.com>
2024-09-12 17:14:07 -0600

    Update proyects.jsx

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -1,6 +1,7 @@
 import { PortfolioItem } from "./portolio-item"
 import { PortfolioItemWorking } from "./portfolio-item-working"
 import TyperTraining from "../assets/TyperTraining.png"
+import outfit from "../assets/outfit.jpeg"
 
 export function Proyects(){
     return(
@@ -15,7 +16,7 @@ export function Proyects(){
                     <h5 className="mb-14 text-white text-[50px] tracking-[10px] font-bold max-md:text-[1.3rem] max-md:mb-[2rem]">MY <span className="text-purple-700">PROJECTS</span></h5>
                     <div className="portolfio-container grid grid-cols-[repeat(3,_1fr)] gap-10 max-md:grid-cols-[repeat(2,_1fr)] max-sm:grid-cols-[repeat(1,_1fr)]">
                         <PortfolioItem title="TyperTraining" imgUrl={TyperTraining} gitUrl="https://github.com/karlobgisw/TyperTraining.git" demoUrl="https://typertraining-production-259c.up.railway.app/"></PortfolioItem>
-                        <PortfolioItemWorking></PortfolioItemWorking>
+                        <PortfolioItem title="Out-Fit" imgUrl={outfit} gitUrl="" demoUrl="https://outfitdemo.netlify.app/"></PortfolioItem>
                         <PortfolioItemWorking></PortfolioItemWorking>
                     </div>
                 </div>
