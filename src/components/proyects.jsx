01707f0e2a313a8d702f8f8a159d2e52358d31d6
karlobgisw <karlongobg@gmail.com>
2024-09-23 11:07:42 -0600

    tipCalculator

--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -2,6 +2,7 @@ import { PortfolioItem } from "./portolio-item"
 import { PortfolioItemWorking } from "./portfolio-item-working"
 import TyperTraining from "../assets/TyperTraining.png"
 import outfit from "../assets/outfit.jpeg"
+import TipCalculator from "../assets/TipCalculator.jpeg"
 
 export function Proyects(){
     return(
@@ -16,8 +17,8 @@ export function Proyects(){
                     <h5 className="mb-14 text-white text-[50px] tracking-[10px] font-bold max-md:text-[1.3rem] max-md:mb-[2rem]">MY <span className="text-purple-700">PROJECTS</span></h5>
                     <div className="portolfio-container grid grid-cols-[repeat(3,_1fr)] gap-10 max-md:grid-cols-[repeat(2,_1fr)] max-sm:grid-cols-[repeat(1,_1fr)]">
                         <PortfolioItem title="TyperTraining" imgUrl={TyperTraining} gitUrl="https://github.com/karlobgisw/TyperTraining.git" demoUrl="https://typertraining-production-259c.up.railway.app/"></PortfolioItem>
-                        <PortfolioItem title="Out-Fit" imgUrl={outfit} gitUrl="https://github.com/karlobgisw/OUT-FIT" demoUrl="https://outfitdemo.netlify.app/"></PortfolioItem>
-                        <PortfolioItemWorking></PortfolioItemWorking>
+                        <PortfolioItem title="Shoping Cart" imgUrl={outfit} gitUrl="https://github.com/karlobgisw/OUT-FIT" demoUrl="https://outfitdemo.netlify.app/"></PortfolioItem>
+                        <PortfolioItem title="Tip Calculator" imgUrl={TipCalculator} gitUrl="https://github.com/karlobgisw/Calculadora-de-propinas.git" demoUrl="https://calculadora-de-totales-y-propina.netlify.app/"></PortfolioItem>
                     </div>
                 </div>
                 <br />
